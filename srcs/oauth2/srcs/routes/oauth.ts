8754cfffc95f6b25aa2c2dabb1bd3b9ea66e0372
RPDJF <ruipaulo.unify@outlook.fr>
2025-05-03 14:24:01 +0200

    fix import module from fastify

--- srcs/oauth2/srcs/routes/oauth.ts
+++ srcs/oauth2/srcs/routes/oauth.ts
@@ -1,9 +1,10 @@
-import { FastifyInstance, FastifyPluginOptions } from 'fastify';
+// import type converts commonjs to module
+import type { FastifyInstance, FastifyPluginOptions } from 'fastify';
 
 // login, callback, logout, me
 
 async function oauthRoutes(app: FastifyInstance, opts: FastifyPluginOptions) {
-	app.get("/", async(req, rep) => {
+	app.get("/login", async(req, rep) => {
 		return {url: `${process.env.OAUTH_AUTHORIZATION}?client_id=${process.env.OAUTH_CLIENT_ID}&redirect_uri=${process.env.OAUTH_REDIRECT}&response_type=code`}
 	});
 }
