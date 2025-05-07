ade8399b338b53700323f8e852b93761b1f0a0b5
IlYAN-FISHERMAN <Ilkay.yanar71@proton.me>
2025-05-07 15:46:31 +0200

    merged with main and cleanup correctly

--- srcs/blockchain/srcs/scripts/deploy.js
+++ srcs/blockchain/srcs/scripts/deploy.js
@@ -1,16 +1,16 @@
 async function main() {
   const [deployer] = await hre.ethers.getSigners();
-  console.log("Déploiement avec le compte :", deployer.address);
+  console.log("Deployed with the account:", deployer.address);
 
   const HelloWorld = await hre.ethers.getContractFactory("helloWorld");
   const hello = await HelloWorld.deploy();
 
   await hello.deployed();
-  console.log("Contrat déployé à :", hello.address);
+  console.log("Contrat deployed à :", hello.address);
 
 	const greeting = await hello.greeting();
-  console.log("Message renvoyé par balance() :", hello.balances(hello.address));
-  console.log("Message renvoyé par reeting() :", greeting);
+  console.log("Message send by balance(): ", hello.balances(hello.address));
+  console.log("Message send by reeting(): ", greeting);
 }
 
 main()
