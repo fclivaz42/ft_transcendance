7de21c5f46e3b390ac1c875d8d137a56af9313d3
RPDJF <ruipaulo.unify@outlook.fr>
2025-05-07 17:49:36 +0200

    Enhance header z-index and refactor navbar structure for improved layout

--- srcs/frontend/srcs/components/navbar/index.ts
+++ srcs/frontend/srcs/components/navbar/index.ts
@@ -10,21 +10,25 @@ interface NavbarProps {
 }
 
 export function createNavbar(props: NavbarProps): HTMLElement {
+  const container = document.createElement("div");
+  container.className = "w-24";
+
   const navBar = document.createElement("nav");
 
-  navBar.className = "h-full w-fit flex flex-grow pt-4 bg-navbar dark:bg-navbar_dark dark:border-panel_dark dark:border-r";
+  navBar.className = "z-0 fixed top-0 bottom-0 flex pt-20 bg-navbar dark:bg-navbar_dark dark:border-panel_dark dark:border-r";
   navBar.id = "navBar";
   navBar.innerHTML = `
-      <div class="flex flex-col flex-grow justify-between w-fit">
-        <div class="flex-col w-fit flex-grow" id="navBarButtons">
-          ${props.buttons.map((option) => `
-            <a class="group w-24 h-24 overflow-visible flex flex-col items-center justify-center gap-y-1 cursor-pointer${option.bottom? " fixed bottom-0" : ""}" id="${option.id}" data-panel="${option.panelId}">
-              ${option.logo ? `<img class="h-8 w-8 dark:invert ${option.animation || " group-hover:animate-squeeze group-hover:animate-duration-300"}" src="${option.logo}">` : ""}
-              <p class="text-nowrap">${option.title}</p>
-            </a>
-          `).join("")}
-        </div>
+    <div class="w-24 flex flex-col flex-grow">
+      <div class="flex-col flex-grow flex" id="navBarButtons">
+        ${props.buttons.map((option) => `
+          <a class="group aspect-square overflow-visible flex flex-col items-center justify-center gap-y-1 cursor-pointer${option.bottom? " mt-auto":""}" id="${option.id}" data-panel="${option.panelId}">
+            ${option.logo ? `<img class="h-8 w-8 dark:invert ${option.animation || " group-hover:animate-squeeze group-hover:animate-duration-300"}" src="${option.logo}">` : ""}
+            <p class="text-nowrap">${option.title}</p>
+          </a>
+        `).join("")}
       </div>
+    </div>
   `;
-  return navBar;
+  container.appendChild(navBar);
+  return container;
 }
