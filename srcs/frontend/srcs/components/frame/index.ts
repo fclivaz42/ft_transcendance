82c2fdd9686992581f9d4aa7699f344a9b47a64d
RPDJF <ruipaulo.unify@outlook.fr>
2025-06-26 20:42:15 +0200

    fix: adjust frame component layout for consistent width and overflow behavior

--- srcs/frontend/srcs/components/frame/index.ts
+++ srcs/frontend/srcs/components/frame/index.ts
@@ -1,7 +1,7 @@
 export default function createFrame(): HTMLElement {
 	const template = document.createElement("template");
 	template.innerHTML = `
-		<main class="p-12 h-full flex-grow bg-background dark:bg-background_dark overflow-auto">
+		<main class="p-12 flex-grow w-full h-full bg-background dark:bg-background_dark overflow-x-auto">
 		</main>
 	`;
 	const frame = template.content.firstElementChild as HTMLElement;
