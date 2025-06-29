d886c4cfdf68e67e473537217f02717f43bfa108
Bravnar <stan.muravyev@bluewin.ch>
2025-06-29 16:32:01 +0200

    chore: small cleanup of PlayerSession, nothing major

--- srcs/games/pong/srcs/plugins/game/classes/PlayerSession.ts
+++ srcs/games/pong/srcs/plugins/game/classes/PlayerSession.ts
@@ -9,12 +9,12 @@ export interface OutgoingMessage {
 
 export default class PlayerSession {
 	private _socket: WebSocket;
-	private _userId: string | null;
+	private _userId: string; // remove | null ;
 	private _playerReady: boolean;
 	private _room: GameRoom | null;
 	private _paddleId: string | null;
 
-	constructor(socket: WebSocket, userId: string | null = null) {
+	constructor(socket: WebSocket, userId: string) { // removed | null = null
 		this._socket = socket;
 		this._userId = userId;
 		this._playerReady = false;
@@ -23,7 +23,7 @@ export default class PlayerSession {
 	}
 
 	public getSocket(): WebSocket { return this._socket; }
-	public getUserId(): string | null { return this._userId; }
+	public getUserId(): string { return this._userId; } // removed | null
 	public isReady(): boolean { return this._playerReady; }
 	public getPaddleId(): string | null { return this._paddleId; }
 	public getRoom(): GameRoom | null { return this._room; }
