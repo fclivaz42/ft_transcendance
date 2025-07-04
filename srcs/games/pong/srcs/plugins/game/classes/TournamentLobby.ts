a06c6174c66800c327b69b8ca16bd4316c17b810
Bravnar <stan.muravyev@bluewin.ch>
2025-07-04 15:55:48 +0200

    feat(backend-game): added TournamentLobby class so that I can broadcast to all players participating in the Tournament, this purely for the countdowns and score updates about all matches, the actual GameRoom broadcasts are still only to the participating players

---
+++ srcs/games/pong/srcs/plugins/game/classes/TournamentLobby.ts
@@ -0,0 +1,43 @@
+import GameRoom from "./GameRoom.ts";
+import PlayerSession from "./PlayerSession.ts";
+
+export type LobbyBroadcastPayload =
+	| { type: "timer"; payload: { secondsRemaining: number } }
+	| {
+			type: "match_result";
+			payload: {
+				roomID: string;
+				winner: string;
+				score: { p1: number; p2: number };
+			};
+	  };
+
+export default class TournamentLobby extends GameRoom {
+	constructor(id: string = "TOURNAMENT_LOBBY") {
+		super(id, false);
+		this.lock = true;
+	}
+
+	public broadcastTimer(secondsRemaining: number): void {
+		this.floodlessBroadcast({
+			type: "timer",
+			payload: { secondsRemaining },
+		});
+	}
+
+	public broadcastMatchResult(
+		roomID: string,
+		winner: string,
+		score: { p1: number; p2: number }
+	): void {
+		this.broadcast({
+			type: "match_result",
+			payload: { roomID, winner, score },
+		});
+	}
+
+	public override addPlayer(playerSession: PlayerSession): void {
+		this.players.push(playerSession);
+		this.broadcast(this.buildPlayerConnectedPayload(playerSession));
+	}
+}
