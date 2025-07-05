0e5cbb13f56b14e183958116b2c6c1db504fd3bc
fclivaz42 <fclivaz@pm.me>
2025-07-05 22:04:51 +0200

    merge ia into 2fa

--- srcs/usermanager/srcs/routes/users/2FAreceipt.ts
+++ srcs/usermanager/srcs/routes/users/2FAreceipt.ts
@@ -6,15 +6,26 @@ import axios from 'axios';
 import https from 'https';
 import DatabaseSDK from '../../../../libs/helpers/databaseSdk.ts';
 
+export var codeUser = new Map<string, string>();
+
+interface Email {
+	email: string,
+};
 
 export default async function twoFaReceiptEndpoint(app: FastifyInstance, opts: FastifyPluginOptions) {
-	app.get("/2fa", async (request, reply) => {
+	app.post<{ Params: Email }>('/2fa/email/:email', async function handler(request, reply) {
 		const authorization = checkRequestAuthorization(request, reply);
 		if (authorization)
 			return authorization;
+		const email = request.params.email;
+		if (!request.headers['code'])
+			return reply.code(400).send("Missing 2fa code");
 
-		const userLogin = request.body as UserLoginProps;
-		const db_sdk = new DatabaseSDK();
-	});
-	return reply.status(200).send({ token: jwtToken.token, ...jwtToken.payload });
+		const code = request.headers['code'];
+		if (!codeUser.get(email))
+			return reply.code(404).send("No 2fa code associated with this email");
+		if (codeUser.get(email) === code)
+			return reply.code(200).send("Authorization");
+		return reply.code(401).send("Verification code is incorrect");
+	})
 }
